e8add3f37590c0257128c2b750cf34bb120f133b
BharatVarma007 <111215544+BharatVarma007@users.noreply.github.com>
2025-07-08 17:30:17 +0530

    Add files via upload
    This is a application folder named "blog" part of Django backend

---
+++ blog_backend/blog/serializers.py
@@ -0,0 +1,35 @@
+from rest_framework import serializers
+from .models import Post, Comment
+from django.contrib.auth.models import User
+from rest_framework.serializers import ModelSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'author', 'content', 'created_at']
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = UserSerializer(read_only=True)
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'author', 'title', 'content', 'created_at', 'comments']
+
+class RegisterSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        return User.objects.create_user(**validated_data)
